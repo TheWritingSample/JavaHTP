23265b8fa798f8b31a5b6807b1867a06b9e885be
Andrew Eroh <andrew.eroh@gmail.com>
2022-04-03 10:55:05 -0400

    First commit Welcome203

---
+++ JavaHTP/src/Welcome203.java
@@ -0,0 +1,9 @@
+//April 03, 2022
+//Printing a line of text with multiple statements.
+public class Welcome203 {
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.print("Welcome to "); //Remove the ln from println to keep text on same line.
+		System.out.println("programming with Java.");
+	} // end method main
+}; // end class Welcome
