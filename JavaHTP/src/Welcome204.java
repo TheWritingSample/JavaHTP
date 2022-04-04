7ab580a9ca96efb7b3d6362b5c1a3ce38439c6e2
Andrew Eroh <andrew.eroh@gmail.com>
2022-04-04 17:06:38 -0400

    First commit for Welcome204, 206, 207

---
+++ JavaHTP/src/Welcome204.java
@@ -0,0 +1,9 @@
+//April 04, 2022
+//Printing multiple lines of text with a single statement.
+
+public class Welcome204 {
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.print("Welcome\nto\nJava\nProgramming!"); 
+	} // end method main
+}; // end class Welcome
---
+++ JavaHTP/src/Welcome206.java
@@ -0,0 +1,9 @@
+//April 04, 2022
+//Displaying multiple lines with method System.out.printf.
+
+public class Welcome206 {
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!"); 
+	} // end method main
+}; // end class Welcome
