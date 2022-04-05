cdc901aa0a830b9ac1a6f2b59a90894f02d0c6a9
Andrew Eroh <andrew.eroh@gmail.com>
2022-04-05 09:07:41 -0400

    First commit for new programs

---
+++ JavaHTP/src/Welcome215.java
@@ -0,0 +1,43 @@
+// April 05, 2022
+// Compare integers using if statements, relational operators
+// and equality operators
+import java.util.Scanner; // program uses class Scanner
+
+public class Welcome215 {
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		// create a Scanner to obtain input from the command window
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter first integer: "); // prompt
+		int number1 = input.nextInt(); // read first number from user
+		
+		System.out.print("Enter second integer: "); // prompt
+		int number2 = input.nextInt(); // read second number from user
+		
+		if (number1 == number2) {
+			System.out.printf("%d == %d%n", number1, number2);
+		}
+		
+		if (number1 != number2) {
+			System.out.printf("%d != %d%n", number1, number2);
+		}
+		
+		if (number1 < number2) {
+			System.out.printf("%d < %d%n", number1, number2);
+		}
+		
+		if (number1 > number2) {
+			System.out.printf("%d > %d%n", number1, number2);
+		}
+		
+		if (number1 <= number2) {
+			System.out.printf("%d <= %d%n", number1, number2);
+		}
+		
+		if (number1 >= number2) {
+			System.out.printf("%d => %d%n", number1, number2);
+		}
+		
+	} // end method main
+}; // end class Welcome
---
+++ JavaHTP/src/Welcome26ASE.java
@@ -0,0 +1,25 @@
+// April 05, 2022
+// Calculate the total marks obtained in three tests
+import java.util.Scanner; // program uses class Scanner
+
+public class Welcome26ASE {
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		// create a Scanner to obtain input from the command window
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter marks in first test: "); // prompt
+		int t1 = input.nextInt(); // read from user
+		
+		System.out.print("Enter marks in first test: "); // prompt
+		int t2 = input.nextInt(); // read from user
+		
+		System.out.print("Enter marks in first test: "); // prompt
+		int t3 = input.nextInt(); // read from user
+		
+		int total = t1 + t2 + t3; // calculate total
+		
+		System.out.printf("Total marks: %d%n", total);
+		
+	} // end method main
+}; // end class 
