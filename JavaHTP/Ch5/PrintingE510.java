d90c72e4e53b4622f1446fd0da946c8dbf44c400
Andrew Eroh <andrew.eroh@gmail.com>
2022-05-24 12:24:03 -0400

    upload new program

---
+++ JavaHTP/Ch5/PrintingE510.java
@@ -0,0 +1,18 @@
+// May 24 2022
+// What does this program do?
+// It prints 112123123412345
+
+public class PrintingE510 {
+	public static void main( String [] args )
+	{
+		for (int i = 1; i <= 5; i++) // count from 1 to 5
+		{
+			for (int j = 1; j <= i; j++) // count from 1 to i
+				System.out.print(j); // output j
+			
+		} // end outer loop
+	
+	
+	}
+
+} // end class PrintingE510
