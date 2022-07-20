9330c3b79d988111265aaaa4b2512e0ef2607f93
Andrew Eroh <andrew.eroh@gmail.com>
2022-07-20 15:55:25 -0400

    software update Welcome202

--- JavaHTP/Ch2/Welcome202.java
+++ JavaHTP/Ch2/Welcome202.java
@@ -4,5 +4,8 @@ public class Welcome202 {
 	// main method begins execution of Java application
 	public static void main(String[] args) {
 		System.out.println("Should I be learning Java? I'm not sure if it's best for me!");
+		System.out.println("I think Jave is great. Who cares if I ever use it for work?!");
+		System.out.println("I just link programming for whatever reason");
+		System.out.println("So keep at it! and you'll understand more over time, even if its slow!");
 	} // end method main
 }; // end class Welcome
