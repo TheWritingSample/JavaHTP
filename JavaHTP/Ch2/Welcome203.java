ac547fb38ca26e6a3cb238d91834234d19c86c0e
Andrew Eroh <andrew.eroh@gmail.com>
2022-07-20 15:57:57 -0400

    SW update

--- JavaHTP/Ch2/Welcome203.java
+++ JavaHTP/Ch2/Welcome203.java
@@ -5,5 +5,7 @@ public class Welcome203 {
 	public static void main(String[] args) {
 		System.out.print("Welcome to "); //Remove the ln from println to keep text on same line.
 		System.out.println("programming with Java.");
+		System.out.print("If I remove the 'l & n' from print,");
+		System.out.println(" it makes the two lines stick together.");
 	} // end method main
 }; // end class Welcome
