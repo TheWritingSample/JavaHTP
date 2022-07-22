00615ae2e628dad7c8123494117b1f8803725d7d
Andrew Eroh <andrew.eroh@gmail.com>
2022-07-22 17:15:00 -0400

    format print experiment

--- JavaHTP/Ch2/Welcome206.java
+++ JavaHTP/Ch2/Welcome206.java
@@ -4,6 +4,7 @@
 public class Welcome206 {
 	// main method begins execution of Java application
 	public static void main(String[] args) {
-		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!"); 
+		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+		System.out.printf("%s%n%s%n", "I like to", "eat tacos.");
 	} // end method main
 }; // end class Welcome
