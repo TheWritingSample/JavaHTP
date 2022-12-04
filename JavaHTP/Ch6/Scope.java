7a3d6415a8b48e147c0077fa62f406b065a5cfde
Andrew Eroh <andrew.eroh@gmail.com>
2022-12-04 08:43:03 -0500

    completed and debugged program

--- JavaHTP/Ch6/Scope.java
+++ JavaHTP/Ch6/Scope.java
@@ -1,10 +1,8 @@
-// June 23 2022   
+// December 04 2022   
 // Scope class demonstrates field and local-variable scopes.
 
-import java.security.SecureRandom; // program uses class SecureRandom
-
 public class Scope {
-	// filed that is accessible to all methods of this class
+	// field that is accessible to all methods of this class
 	private static int x = 1; // private static integer 
 	
 	// method main creates and initializes local variable x
@@ -13,9 +11,29 @@ public class Scope {
 		int x = 5; // method's local variable x shadows field x
 		
 		System.out.printf("local x in main is %d%n", x);
+		useLocalVariable(); // useLocalVariable has local x
+		useField(); // useField uses class Scope's field x
+		useLocalVariable(); // useLocalVariable reinitializes local x
+		useField(); // class Scope's filed x retains its value
 		
+		System.out.printf("%nlocal x in main is %d%n", x);
+	}
+	
+	// create and initialize local variable x during each call
+	public static void useLocalVariable() {
+		int x = 25; // initialized each time useLocalVariable is called
 		
-		
-		
+		System.out.printf("%nlocal x on entering method useLocalVariable is %d%n", x);
+		++x; // modifies this method's local variable x
+		System.out.printf("local x before exiting method useLocalVariable is %d%n", x);
+	}
+	
+	// modify class Scope's field x during each call 
+	public static void useField() {
+		System.out.printf(
+				"%nfiield x on entering method useField is %d%n", x);
+				x *= 10; // modifies class Scope's field x"
+				System.out.printf(
+				"field x before exiting method useField is %d%n", x);
 	}
 }
