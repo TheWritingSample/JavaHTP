da5d9e842dc37983d02003bf0929e96bd54263cb
Andrew Eroh <andrew.eroh@gmail.com>
2022-12-14 21:32:58 -0500

    added print line

--- JavaHTP/Ch6/MethodOverLoad.java
+++ JavaHTP/Ch6/MethodOverLoad.java
@@ -5,6 +5,7 @@ public class MethodOverLoad {
 	public static void main (String[] args) {
 		System.out.printf("Square of integer 7 is %d%n", square(7));
 		System.out.printf("Square of double 7.5 is %f%n", square(7.5));
+		System.out.printf("Square of integer 7 is %d%n", square(8));
 	}
 	
 	// square method with int argument
