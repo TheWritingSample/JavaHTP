7b924953646367c32f20233a2de2773b65323b48
Andrew Eroh <andrew.eroh@gmail.com>
2022-12-19 09:45:55 -0500

    Add Tree program

---
+++ JavaHTP/CCL/Tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // This program introduces me
+    // and prints a tree on the screen
+
+    System.out.println("Hello, I am Andrew and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("   *   ");
+    System.out.println("  ***  ");
+    System.out.println("  ***  ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+  } 
+
+}
